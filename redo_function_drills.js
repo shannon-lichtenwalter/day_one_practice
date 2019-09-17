b493f57515f9ca901172bb57ac95f520d5beb353
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-16 19:59:16 -0600

    year of birth drill

---
+++ redo_function_drills.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//greeting function
+
+function greeting(name, age) {
+  if (name === 'undefined' || age === 'undefined'){
+    throw new Error('Arguments not valid');
+  }
+  if (typeof name !== 'string' || typeof age !== 'number'){
+    throw new Error ('invalid entry');
+  }
+  const year = getYearOfBirth(age); 
+  return `Hi my name is ${name} and I am ${age}. I was born in ${year}.`;
+}
+
+function getYearOfBirth(age){
+  if (age < 0){
+    throw new Error('Age can not be negative');
+  }
+  return 2019 - age;
+}
+
+try{
+  const greeting1 = greeting('Shannon', 27);
+  console.log(greeting1);
+} catch (err) {
+  console.log(err);
+}
