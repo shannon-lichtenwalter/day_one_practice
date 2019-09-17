cfd22980bb0973fa5940a3e8bcabacf34f5189ab
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-16 18:31:53 -0600

    for and while loop basics

---
+++ practice.js
@@ -0,0 +1,17 @@
+'use strict';
+// practicing for loops. best used when there is a set of 
+// operations that you want to do a set number of times.
+
+// const countTo = 10;
+// for (let i=1; i <= countTo; i++){
+//   console.log(i);
+// }
+
+//while loop
+
+let counter = 1;
+const countTo = 10;
+while (counter <= countTo) {
+  console.log(counter);
+  counter++;
+}
